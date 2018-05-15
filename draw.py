43cde0624114030a998187215f6a2fad362b2f4b
johnnyWzq <vict_cfy@163.com>
2018-05-15 22:39:01 +0800

    add the search keyword 'station_id'

--- draw.py
+++ draw.py
@@ -10,13 +10,12 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as dt
-import matplotlib.ticker as mtick
-import datetime
+
+import re
 from matplotlib.gridspec import GridSpec
 
-from matplotlib.dates import DayLocator, HourLocator, MinuteLocator
+from matplotlib.dates import HourLocator, MinuteLocator
 
-from preprocess_charging_data import read_csv_file
 
 def sel_station(data, num):
     df = data[:]
@@ -51,10 +50,10 @@ def draw_loads(xy_dict, file_name, data_dir):
         x = y_mean.index.tolist()
         x = list(map(lambda k:k[5:], x))
         x = pd.to_datetime(x, format='%H:%M:%S')
+        
+        df = pd.DataFrame({'x':x, 'y_mean':y_mean.values, 'y_max':y_max.values})
+        df.to_csv(os.path.join(p_data_dir, '%s.csv'%station_id), encoding='gb18030')
         """
-        #df = pd.DataFrame({'x':x, 'y_mean':y_mean.values, 'y_max':y_max.values, 'y_min':y_min.values})
-        #df.to_csv(os.path.join(p_data_dir, 'CAAHAA000102.csv'), encoding='gb18030')
-    
         xy = pd.read_csv(os.path.join(p_data_dir, 'CAAHAA000102.csv'), encoding='gb18030')
         y_mean = xy['y_mean']
         y_max = xy['y_max']
@@ -68,8 +67,8 @@ def draw_loads(xy_dict, file_name, data_dir):
         ax = plt.subplot(gs[ax_num, 0], facecolor='#383737')
         ax_num += 1
         
-        ax.plot(y_mean, label='平均负载')
-        ax.plot(y_max, label='最大负载')
+        ax.plot(x, y_mean, label='平均负载')
+        ax.plot(x, y_max, color='silver', label='最大负载')
         #ax.plot(x, y_min, label='最小负载')
         ax.legend(loc='upper right')
         ax.set_xlim(x[0], x[-1])
@@ -89,14 +88,14 @@ def draw_loads(xy_dict, file_name, data_dir):
 if __name__ == '__main__':
     data_dir = os.path.join(os.path.abspath('.'), 'load_data')
     p_data_dir = os.path.join(os.path.abspath('.'), 'load_pic')
-    
-    data = read_csv_file(data_dir, 'Loads.csv')
-
+    data = pd.read_csv(os.path.join(data_dir, 'Loads.csv'), error_bad_lines=False,
+                                    encoding='gb18030')
     #num = 3
-    for num in range(1):
-        station_id = sel_station(data, num)
-        #station_id = 'CAAHAA000102'
-        years = [2017]
-        xy = sel_xy(data, station_id, *years)
-        
-        draw_loads(xy, station_id, p_data_dir)
+    #for num in range(1):#range(10, 50):
+    for station_id in data['station_id'].tolist():
+        #station_id = sel_station(data, num)
+        if re.match(r'PHCAGDB\d+', station_id):
+            years = [2017]
+            xy = sel_xy(data, station_id, *years)
+            
+            draw_loads(xy, station_id, p_data_dir)
