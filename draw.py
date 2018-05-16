41245ab59d0da2bc6d0b5f39ee59c5e95191b7cb
johnnyWzq <vict_cfy@163.com>
2018-05-16 22:58:26 +0800

    add the cluster_model

--- draw.py
+++ draw.py
@@ -10,12 +10,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as dt
-
+from dateutil import parser 
 import re
 from matplotlib.gridspec import GridSpec
-
 from matplotlib.dates import HourLocator, MinuteLocator
-
+from preprocess_charging_data import time_list
 
 def sel_station(data, num):
     df = data[:]
@@ -44,15 +43,16 @@ def draw_loads(xy_dict, file_name, data_dir):
         file_name += '_%d'%k
         
         y_mean = xy.iloc[0][xy.apply(str).str.contains('mean_',regex=False)]#取所有column为mean_的列
-        #y_median = xy.str.contains('median_', regex=False)
+        y_median = xy.iloc[0][xy.apply(str).str.contains('median_',regex=False)]
         y_max = xy.iloc[0][xy.apply(str).str.contains('max_',regex=False)]
-        #y_min = xy.iloc[0][xy.apply(str).str.contains('min_',regex=False)]
+        y_75 = xy.iloc[0][xy.apply(str).str.contains('75%_',regex=False)]
+        y_25 = xy.iloc[0][xy.apply(str).str.contains('25%_',regex=False)]
         x = y_mean.index.tolist()
         x = list(map(lambda k:k[5:], x))
         x = pd.to_datetime(x, format='%H:%M:%S')
         
-        df = pd.DataFrame({'x':x, 'y_mean':y_mean.values, 'y_max':y_max.values})
-        df.to_csv(os.path.join(p_data_dir, '%s.csv'%station_id), encoding='gb18030')
+        #df = pd.DataFrame({'x':x, 'y_mean':y_mean.values, 'y_max':y_max.values})
+        #df.to_csv(os.path.join(p_data_dir, '%s.csv'%station_id), encoding='gb18030')
         """
         xy = pd.read_csv(os.path.join(p_data_dir, 'CAAHAA000102.csv'), encoding='gb18030')
         y_mean = xy['y_mean']
@@ -69,7 +69,9 @@ def draw_loads(xy_dict, file_name, data_dir):
         
         ax.plot(x, y_mean, label='平均负载')
         ax.plot(x, y_max, color='silver', label='最大负载')
-        #ax.plot(x, y_min, label='最小负载')
+        ax.plot(x, y_median, label='中位数负载')
+        ax.plot(x, y_75, label='75%位数负载')
+        ax.plot(x, y_25, label='25%位数负载')
         ax.legend(loc='upper right')
         ax.set_xlim(x[0], x[-1])
         ax.grid(linestyle=':') #开启网格
@@ -84,6 +86,61 @@ def draw_loads(xy_dict, file_name, data_dir):
     fig.savefig(os.path.join(data_dir, '%s.jpg'%file_name), dpi=128)
     fig.show()
     
+def draw_loads_type(xy_dict, file_name, data_dir):
+    fig = plt.figure(edgecolor='k',figsize=(16, 28))
+    plt.rcParams['font.sans-serif'] = ['SimHei'] #用来正常显示中文标签
+    plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
+    
+    ax_num = len(xy_dict)
+    gs = GridSpec(ax_num, 1)
+    ax_num = 0
+    for k, xy in xy_dict.items():
+        file_name += '_%s'%k 
+        print('calculating the data of axis_y...')
+        t1 = '00:00:00'
+        t2 = '23:59:59'
+        func =  lambda x:parser.parse(x)
+        cols = time_list(func(t1), func(t2))
+        y_mean = xy[list(map(lambda x:'mean_'+x, cols))]
+        y_median = xy[list(map(lambda x:'median_'+x, cols))]
+        y_max = xy[list(map(lambda x:'max_'+x, cols))]
+        y_25 = xy[list(map(lambda x:'25%_'+x, cols))]
+        y_75 = xy[list(map(lambda x:'75%_'+x, cols))]
+
+        x = pd.to_datetime(cols, format='%H:%M:%S')
+        first_time = True
+        for cnt in range(len(xy)):
+            
+            print('drawing the part%d of %s picture...'%(cnt, file_name))
+            if first_time:
+                ax = plt.subplot(gs[ax_num, 0], facecolor='#383737')
+                ax.plot(x, y_mean.iloc[cnt], label='平均负载')
+                ax.plot(x, y_max.iloc[cnt], color='silver', label='最大负载')
+                ax.plot(x, y_median.iloc[cnt], label='中位数负载')
+                ax.plot(x, y_25.iloc[cnt], label='25%位负载')
+                ax.plot(x, y_75.iloc[cnt], label='75%位负载')
+                ax.legend(loc='upper right')
+                first_time = False
+            else:
+                ax = plt.subplot(gs[ax_num, 0], facecolor='#383737')
+                ax.plot(x, y_mean.iloc[cnt])
+                ax.plot(x, y_max.iloc[cnt])
+                ax.plot(x, y_median.iloc[cnt])
+                ax.plot(x, y_25.iloc[cnt])
+                ax.plot(x, y_75.iloc[cnt])
+        ax.grid(linestyle=':') #开启网格
+        ax.set_ylabel('负荷功率 (kwh)')
+        ax.set_title('%s场站24小时负荷曲线'%k)
+        ax.set_xlim(x[0], x[-1])
+        ax.xaxis.set_major_locator(HourLocator())
+        ax.xaxis.set_minor_locator(MinuteLocator(range(0, 60, 15)))
+        ax.xaxis.set_major_formatter(dt.DateFormatter('%H:%M:%S'))
+
+        ax_num += 1
+        
+    fig.autofmt_xdate()
+    fig.savefig(os.path.join(data_dir, '%s.jpg'%file_name), dpi=128)
+    fig.show()
 
 if __name__ == '__main__':
     data_dir = os.path.join(os.path.abspath('.'), 'load_data')
